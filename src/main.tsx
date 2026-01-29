fd708d461e4cc362b94a2599956f4aae0eee3530
jwheelED <jwheel01@nyit.edu>
2026-01-29 17:12:28 +0000

    Setup PostHog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,20 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx' // I kept this as .tsx to match your file
+import { PostHogProvider } from 'posthog-js/react'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const options = {
+  api_host: 'https://us.i.posthog.com', // Change to 'https://eu.i.posthog.com' if in Europe
+}
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <PostHogProvider 
+      apiKey="phc_vRUtKXaLgYpSzc9H4jOmN2fsc72gn39wsRDx0IZspxq" 
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
+  </StrictMode>
+)
